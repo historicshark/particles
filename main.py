0d360ba7f88a730a07ea38b3fd2131ffc3aac66a
HISTORICSHARK\mgs84 <historicshark98@gmail.com>
2024-04-18 08:46:23 -0400

    walls, permutations

--- main.py
+++ main.py
@@ -4,53 +4,55 @@ import matplotlib.animation as animation
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import itertools
 
 from particle import Particle
+
 
 def main():
     # --------------------------------------------
     # Inputs
     # --------------------------------------------
-    
+
     # Fluid
     gx = 0
     gy = -9.81
     mu_l = 1
-    rho_l = 1
-    
+    rho_l = 0
+
     # Particles
-    n_particles = 3
-    
-    diameter = 5
-    diameter_stddev = .5
+    n_particles = 2
+
+    diameter = 10
+    diameter_stddev = 0
     diameter_min = 1
-    
+
     velocity = 0
-    velocity_stddev = 15
-    
+    velocity_stddev = 0
+
     rho_p = 1
-    
-    epsilon = 1
-    
+
+    epsilon = .9
+
     drag = False
-    gravity = False
+    gravity = True
     wall_collisions = True
-    
+
     # Domain
     nx = 100
     ny = 100
-    
+
     xmin = -100
     xmax = 100
     ymin = -100
     ymax = 100
-    
+
     # Time
     dt = 0.001
-    end_time = 20
+    end_time = 100
     n_frames = 800
     save_animation = False
-    
+
     # --------------------------------------------
     # Particles
     # --------------------------------------------
@@ -59,168 +61,213 @@ def main():
     # y0 = np.array([0, 0])
     # u0 = np.array([-15,15])
     # v0 = np.array([0,0])
-    
+
     rng = np.random.default_rng()
-    rp = rng.normal(diameter/2, diameter_stddev/2, (n_particles,))
-    rp[rp < diameter_min/2] = diameter_min/2
-    
+    rp = rng.normal(diameter / 2, diameter_stddev / 2, (n_particles,))
+    rp[rp < diameter_min / 2] = diameter_min / 2
+
     rho = np.full((n_particles,), rho_p)
-    
+
     middle = 0.9
     x0 = middle * ((xmax - xmin) * rng.random((n_particles,)) - (xmax - xmin) / 2)
     y0 = middle * ((ymax - ymin) * rng.random((n_particles,)) - (ymax - ymin) / 2)
-    
+
     u0 = rng.normal(velocity, velocity_stddev, (n_particles,))
     v0 = rng.normal(velocity, velocity_stddev, (n_particles,))
-    
+
     def distance(x1, y1, x2, y2):
-        return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-    
-    #   Check initially overlapping particles. Just delete one of them for now
-    particles_to_keep = np.full(rp.shape, True)
-    n_particles_removed = 0
-    for i in range(n_particles):
-        for j in range(n_particles):
-            if i != j:
-                if particles_to_keep[i] and particles_to_keep[j] and distance(x0[i], y0[i], x0[j], y0[j]) < rp[i] + rp[j]:
-                    particles_to_keep[j] = False
-                    print(f'{i = }, {j = }')
-                    n_particles_removed += 1
-    n_particles -= n_particles_removed
-    
-    print(f'Removed {n_particles_removed} initially overlapping particles')
-    print(f'{particles_to_keep = }')
-    
-    rp = rp[particles_to_keep]
-    rho = rho[particles_to_keep]
-    x0 = x0[particles_to_keep]
-    y0 = y0[particles_to_keep]
-    u0 = u0[particles_to_keep]
-    v0 = v0[particles_to_keep]
-    
+        return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+    # Check initially overlapping particles. Just delete one of them for now
+    if n_particles > 1:
+        particles_to_keep = np.full(rp.shape, True)
+        n_particles_removed = 0
+        for i in range(n_particles):
+            for j in range(n_particles):
+                if i != j:
+                    if particles_to_keep[i] and particles_to_keep[j] and distance(x0[i], y0[i], x0[j], y0[j]) < rp[i] + rp[j]:
+                        particles_to_keep[j] = False
+                        print(f'{i = }, {j = }')
+                        n_particles_removed += 1
+        n_particles -= n_particles_removed
+
+        print(f'Removed {n_particles_removed} initially overlapping particles')
+
+        rp = rp[particles_to_keep]
+        rho = rho[particles_to_keep]
+        x0 = x0[particles_to_keep]
+        y0 = y0[particles_to_keep]
+        u0 = u0[particles_to_keep]
+        v0 = v0[particles_to_keep]
+    else:
+        x0 = np.zeros((n_particles,))
+        y0 = np.zeros((n_particles,))
+
     position = np.stack((x0, y0), axis=1)
     velocity = np.stack((u0, v0), axis=1)
     v_l = np.zeros((2,))
-    g = np.array([gx,gy])
-    
+    g = np.array([gx, gy])
+
     particles = []
     for i in range(n_particles):
-        particles.append(Particle(i, rp[i], epsilon, rho[i], position[i], velocity[i], rho_l, mu_l, v_l, g, drag, gravity, wall_collisions))
-    
+        particles.append(
+            Particle(i, rp[i], epsilon, rho[i], position[i], velocity[i], rho_l, mu_l, v_l, g, drag, gravity, wall_collisions))
+
     # --------------------------------------------
     # Domain
     # --------------------------------------------
     walls = [xmax, xmin, ymax, ymin]
-    
+
     x = np.linspace(xmin, xmax, nx)
     y = np.linspace(ymin, ymax, ny)
-    
+
     X, Y = np.meshgrid(x, y)
-    
+
     # --------------------------------------------
     # Run calculations
     # --------------------------------------------
     cwd = Path.cwd()
-    
+
     domain_filepath = cwd.joinpath('domain.csv')
     particle_filepath = cwd.joinpath('particles.csv')
     time_filepath = cwd.joinpath('time.csv')
     position_filepath = cwd.joinpath('position.csv')
     velocity_filepath = cwd.joinpath('velocity.csv')
     results_filepath = cwd.joinpath('results.csv')
-    
+
     print('Running calculation...')
-    
+
     calculate(particles, walls, dt, end_time, time_filepath, position_filepath, velocity_filepath, results_filepath)
-    
+
     print('Finished calculations')
-    
+
     # --------------------------------------------
     # Animation
     # --------------------------------------------
     animate(time_filepath, position_filepath, velocity_filepath, results_filepath, rp, xmin, xmax, ymin, ymax, n_frames, save_animation)
 
-def calculate(particles, walls, dt, end_time, time_filepath, position_filepath, velocity_filepath, results_filepath):
+
+def calculate(particles, walls, dt, end_time, time_filepath, position_filepath, velocity_filepath, results_filepath) -> None:
     time = np.arange(0, end_time, dt)
-    
+
     # position_file = open(position_filepath, 'w')
     # velocity_file = open(velocity_filepath, 'w')
     # results_file = open(results_filepath, 'w')
-    
+
     pd.DataFrame(time).to_csv(time_filepath, header=None)
-    
+
+    n_substeps = 10
+    dt_substeps = dt / n_substeps
+
+    particle_combinations = []
+    for permutation in itertools.permutations(particles, 2):
+        if (permutation[1], permutation[0]) not in particle_combinations:
+            particle_combinations.append(permutation)
+
+    print('Combinations:')
+    for (particle, other) in particle_combinations:
+        print(f'\t{particle.id}, {other.id}')
+
     with open(position_filepath, 'w') as position_file, open(velocity_filepath, 'w') as velocity_file, open(results_filepath, 'w') as results_file:
         results_file.write('ke,pe,me\n')
         for t in time:
             ke = 0.0
             pe = 0.0
             me = 0.0
-            
+
             for particle in particles:
                 particle.collided = False
-            
+                # particle.finished_update = False # In integrate
+
             for particle in particles:
-                particle.integrate_update(dt, walls)
-                
+                particle.integrate(dt)
+
+                # If particle hits a wall, reduce time step to integrate up to collision
+                if particle.detect_wall_contact(walls):
+                    i_substep = 0
+                    keepgoing = True
+                    while keepgoing:
+                        particle.integrate(dt_substeps)
+                        i_substep += 1
+                        if i_substep > n_substeps or particle.detect_wall_contact(walls):
+                            keepgoing = False
+                        else:
+                            particle.update()
+
+                    dt_remaining = dt - dt_substeps * i_substep
+                    particle.integrate(dt_remaining)
+
+                particle.update()
+
+            # particle collision detection
+            for particle, other in particle_combinations:
+                pass
+
+            for particle in particles:
                 ke += particle.kinetic_energy()
-                
+                pe += particle.potential_energy(walls[3])
+                me += particle.mechanical_energy(walls[3])
+
                 position_file.write(particle.position_string() + ',')
                 velocity_file.write(particle.velocity_string() + ',')
-            
+
             position_file.write('\n')
             velocity_file.write('\n')
             results_file.write(f'{ke},{pe},{me}\n')
-    
+
     return
 
-def animate(time_file, position_file, velocity_file, results_file, rp, xmin, xmax, ymin, ymax, n_frames, save):
+
+def animate(time_file, position_file, velocity_file, results_file, rp, xmin, xmax, ymin, ymax, n_frames, save) -> None:
     cwd = Path.cwd()
-    
+
     time = pd.read_csv(time_file, header=None)
     position = pd.read_csv(position_file, header=None)
     # velocity = pd.read_csv(velocity_file, header=None)
     results = pd.read_csv(results_file)
-    
-    fig, (ax, ax_bar) = plt.subplots(2,1, height_ratios=[10,1])
-    wall_x = (xmin,xmax,xmax,xmin,xmin)
-    wall_y = (ymin,ymin,ymax,ymax,ymin)
+
+    fig, (ax, ax_bar) = plt.subplots(2, 1, height_ratios=[10, 1])
+    wall_x = (xmin, xmax, xmax, xmin, xmin)
+    wall_y = (ymin, ymin, ymax, ymax, ymin)
     walls = ax.plot(wall_x, wall_y, '')
-    
+
     theta = np.linspace(0, 2 * np.pi)
     cos_theta = np.cos(theta)
     sin_theta = np.sin(theta)
-    
+
     particles = []
-    particle_xy_plot = lambda i, r, time_step: (r * cos_theta + position[2*i][time_step], r * sin_theta + position[2*i+1][time_step])
+    particle_xy_plot = lambda i, r, time_step: (
+    r * cos_theta + position[2 * i][time_step], r * sin_theta + position[2 * i + 1][time_step])
     for i, r in enumerate(rp):
         x_particle, y_particle = particle_xy_plot(i, r, 0)
         particles.append(ax.plot(x_particle, y_particle, '-k')[0])
     ax.axis('equal')
-    ax.set(xlim=(xmin, xmax), ylim=(xmin,ymax))
-    
-    bar, = ax_bar.barh('KE', results['ke'][0])
-    ax_bar.set_xlim(results['ke'].min(), results['ke'].max())
-    
+    ax.set(xlim=(xmin, xmax), ylim=(xmin, ymax))
+
+    bar, = ax_bar.barh('ME', results['me'][0])
+    ax_bar.set_xlim(results['me'].min(), results['me'].max())
+
     def update(frame):
         for i, (r, particle) in enumerate(zip(rp, particles)):
             x_particle, y_particle = particle_xy_plot(i, r, frame)
             particle.set_xdata(x_particle)
             particle.set_ydata(y_particle)
-        bar.set_width(results['ke'][frame])
+        bar.set_width(results['me'][frame])
         return particles
-    
+
     n_time = len(time)
     frames = np.linspace(0, n_time - 1, n_frames).astype(int)
     ani = animation.FuncAnimation(fig=fig, func=update, frames=frames, interval=1)
-    
+
     if save:
         print('Saving animation...')
         ani.save(filename=cwd.joinpath('animation.gif'), writer="pillow")
         print(f"Animation saved to '{cwd.joinpath('animation.gif')}'")
     else:
         plt.show()
-    
+
     return
+
 
 if __name__ == '__main__':
     main()
--- particle.py
+++ particle.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Particle:
     def __init__(self, id_, r, epsilon, rho, x, u, rho_l, mu_l, v_l, g, drag, grav, wall):
         self.id = id_
@@ -20,6 +19,10 @@ class Particle:
         self.wall = wall
         self.collided = False
         self.reynolds_number(self.u)
+        self.wall_collision = np.full((4,), False)
+        self.wall_contact = np.full((4,), False)
+        self.a_collision = 0.0
+        self.finished_update = False
 
     def diameter(self):
         return 2 * self.r
@@ -47,6 +50,12 @@ class Particle:
     def kinetic_energy(self):
         return 0.5 * self.mass() * (self.u @ self.u)
 
+    def potential_energy(self, ymin):
+        return self.mass() * -self.g[1] * (self.x[1] - ymin)
+
+    def mechanical_energy(self, ymin):
+        return self.kinetic_energy() + self.potential_energy(ymin)
+
     def drag_coefficient(self):
         if self.re > 1e-10:
             return 24.0 / self.re * (2.0 / 3.0 + (12.0 / self.re + 3.0 / 4.0 * (1 + 3.315 / np.sqrt(self.re)) ** (-1)))
@@ -56,18 +65,30 @@ class Particle:
     def drag_acceleration(self, u_p):
         self.reynolds_number(u_p)
         v_rel = self.v_l - u_p
-        return 1.0 / 2.0 * self.rho_l / self.rho * np.sqrt(v_rel @ v_rel) * v_rel * self.area_xs() / self.volume() * self.drag_coefficient()
+        return 1.0 / 2.0 * self.rho_l / self.rho * np.sqrt(
+            v_rel @ v_rel) * v_rel * self.area_xs() / self.volume() * self.drag_coefficient()
 
     def buoyancy_acceleration(self):
         return (self.rho - self.rho_l) / self.rho * self.g
 
-    def wall_contact_acceleration(self, dt, walls):
-        a = np.zeros((2,))
-        if self.r > walls[0] - self.x[0] or self.r > self.x[0] - walls[1]:
-            a[0] += -(1 + self.epsilon) * self.u[0] / dt
-        if self.r > walls[2] - self.x[1] or self.r > self.x[1] - walls[3]:
-            a[1] += -(1 + self.epsilon) * self.u[1] / dt
+    def detect_wall_contact(self, walls):
+        self.wall_collision[:] = False
+        if self.r > walls[0] - self.x[0]:
+            self.wall_collision[0] = True
+        if self.r > self.x[0] - walls[1]:
+            self.wall_collision[1] = True
+        if self.r > walls[2] - self.x[1]:
+            self.wall_collision[2] = True
+        if self.r > self.x[1] - walls[3]:
+            self.wall_collision[3] = True
+        return np.any(self.wall_collision)
 
+    def wall_contact_acceleration(self, dt):
+        a = np.zeros((2,))
+        if self.wall_collision[0] or self.wall_collision[1]:
+            a[0] += -(1 + self.epsilon) * self.u[0] / dt
+        if self.wall_collision[2] or self.wall_collision[3]:
+            a[1] += -(1 + self.epsilon) * self.u[1] / dt
         return a
 
     def collision_acceleration(self, other, dt):
@@ -75,32 +96,40 @@ class Particle:
         n = dr / np.sqrt(dr @ dr)
         # dv = np.dot(other.u - self.u, n)
         # m_ratio = other.mass() / (other.mass() + self.mass())
-        u_prime = self.u + n * np.dot(other.u - self.u, n) * (1 + self.epsilon) * other.mass() / (other.mass() + self.mass())
+        u_prime = self.u + n * np.dot(other.u - self.u, n) * (1 + self.epsilon) * other.mass() / (
+                other.mass() + self.mass())
         self.a_collision = (u_prime - self.u) / dt
         return
 
-    def apply_accelerations(self, u_p, dt, walls, collision: bool):
+    def apply_accelerations(self, u_p, dt, collision: bool):
         a = np.zeros((2,))
-        if self.drag: a += self.drag_acceleration(u_p)
-        if self.grav: a += self.buoyancy_acceleration()
-        if self.wall: a += self.wall_contact_acceleration(dt, walls)
-        if collision: a += self.a_collision
+        if self.drag:
+            a += self.drag_acceleration(u_p)
+        if self.grav:
+            a += self.buoyancy_acceleration()
+        if self.wall:
+            a += self.wall_contact_acceleration(dt)
+        if collision:
+            a += self.a_collision
         return a
 
-    def integrate(self, dt, walls, collision=False):
-        a1 = self.apply_accelerations(self.u_n, dt, walls, collision)
+    def integrate(self, dt, collision=False):
+        a1 = self.apply_accelerations(self.u_n, dt, collision)
 
         self.u = self.u_n + dt * a1
         self.x = self.x_n + dt * self.u
+
+        self.finished_update = False
         return
 
     def update(self):
         self.x_n = self.x
         self.u_n = self.u
+        self.finished_update = True
         return
 
-    def integrate_update(self, dt, walls, collision=False):
-        self.integrate(dt, walls, collision)
+    def integrate_update(self, dt, collision=False):
+        self.integrate(dt, collision)
         self.update()
         return
 
